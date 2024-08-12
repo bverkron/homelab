258cc040e1ee267e2935ea3bdde053ed115ca0c6
Ben <>
2024-08-11 17:12:53 -0700

    Additional nas share monitoring logging

--- scripts/hosts/monitoring/nas_share_monitoring.sh
+++ scripts/hosts/monitoring/nas_share_monitoring.sh
@@ -1,7 +1,8 @@
-touch /mnt/ReplicateOut_master_control/monitoring/up.txt;
+echo echo $(date '+%Y-%m-%d %H:%M:%S') Network share check > /mnt/ReplicateOut_master_control/monitoring/up.txt
+
 if [ $? -eq 0 ]; then
-    echo OK
+    echo $(date '+%Y-%m-%d %H:%M:%S') Network share OK
     curl "http://vector.ournetwork.ca:3001/api/push/yISAMDTRaC?status=up&msg=OK&ping="
 else
-    echo FAIL
+    echo $(date '+%Y-%m-%d %H:%M:%S') Network share FAILED
 fi
