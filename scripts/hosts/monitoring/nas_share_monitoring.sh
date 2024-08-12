950c0f6118992cdbeeb15cf652c2d8d31e002ae5
Ben <>
2024-08-11 17:45:35 -0700

    Update uptime-kuma ping url

--- scripts/hosts/monitoring/nas_share_monitoring.sh
+++ scripts/hosts/monitoring/nas_share_monitoring.sh
@@ -2,7 +2,7 @@ echo echo $(date '+%Y-%m-%d %H:%M:%S') Network share check > /mnt/ReplicateOut_m
 
 if [ $? -eq 0 ]; then
     echo $(date '+%Y-%m-%d %H:%M:%S') Network share OK
-    curl "http://vector.ournetwork.ca:3001/api/push/yISAMDTRaC?status=up&msg=OK&ping="
+    curl "https://uptime-kuma.ournetwork.ca/api/push/yISAMDTRaC?status=up&msg=OK&ping="
 else
     echo $(date '+%Y-%m-%d %H:%M:%S') Network share FAILED
 fi
