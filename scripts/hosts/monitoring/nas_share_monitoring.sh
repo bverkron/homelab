aad0e6c99c89438aa194d0ce75bf16dd7f4c20fb
Ben <>
2024-08-16 11:12:43 -0700

    comments

--- scripts/hosts/monitoring/nas_share_monitoring.sh
+++ scripts/hosts/monitoring/nas_share_monitoring.sh
@@ -1,3 +1,7 @@
+# If all infra needs to be shutdown, for example if there is power outage or other maintenance happening in the server room
+#   then the NAS may not be up when this server boots up. Thus the monitoring script can be used to attempt to re-mount the shared.
+#   Tested by using sudo unmount -l /mnt/ReplicateOut_master_control/ and then running this script. It reconnected the share as expected.
+#   Have not tested the failure scenario where NAS is offline when this run, for example.
 echo $(date '+%Y-%m-%d %H:%M:%S') Network share check > /mnt/ReplicateOut_master_control/monitoring/up.txt
 
 if [ $? -eq 0 ]; then
