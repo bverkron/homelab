2cbc161110252b1930362da2e02add45f2a81a44
Ben <>
2024-08-16 11:10:59 -0700

    Improve NAS share monitoring so it attempts to re-mount if not connected.

--- scripts/hosts/monitoring/nas_share_monitoring.sh
+++ scripts/hosts/monitoring/nas_share_monitoring.sh
@@ -1,8 +1,18 @@
-echo echo $(date '+%Y-%m-%d %H:%M:%S') Network share check > /mnt/ReplicateOut_master_control/monitoring/up.txt
+echo $(date '+%Y-%m-%d %H:%M:%S') Network share check > /mnt/ReplicateOut_master_control/monitoring/up.txt
 
 if [ $? -eq 0 ]; then
     echo $(date '+%Y-%m-%d %H:%M:%S') Network share OK
     curl "https://uptime-kuma.ournetwork.ca/api/push/yISAMDTRaC?status=up&msg=OK&ping="
 else
     echo $(date '+%Y-%m-%d %H:%M:%S') Network share FAILED
-fi
+    echo $(date '+%Y-%m-%d %H:%M:%S') Attempting to mount...
+    sudo mount -a
+    echo $(date '+%Y-%m-%d %H:%M:%S') Network share check after re-mount > /mnt/ReplicateOut_master_control/monitoring/up.txt
+    if [ $? -eq 0 ]; then
+        echo $(date '+%Y-%m-%d %H:%M:%S') Network share mounted OK
+        curl "https://uptime-kuma.ournetwork.ca/api/push/yISAMDTRaC?status=up&msg=OK&ping="
+    else
+        echo $(date '+%Y-%m-%d %H:%M:%S') Network mount FAILED
+        curl "https://uptime-kuma.ournetwork.ca/api/push/yISAMDTRaC?status=down&msg=Mount_failed&ping="
+    fi
+fi 
