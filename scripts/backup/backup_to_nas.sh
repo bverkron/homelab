3c0d3e02a8420db782e1b0c3677597d9f1d47bde
Ben <>
2024-09-21 16:27:50 -0700

    Fix rsync command

--- scripts/backup/backup_to_nas.sh
+++ scripts/backup/backup_to_nas.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-SHARE=ReplicateOut && screen -S background -d -L -Logfile "/tmp/rsync-$(date +%Y-%m-%d_%H-%M-%S.txt)" -m rsync -rtERvv --delete --exclude=".*/" -e "ssh -i /root/.ssh/ided25519_qnap_nas" "admin@qnap-nas.ournetwork.ca:/share/MD0_DATA/./$SHARE/" /mnt/user/
+SHARE=ReplicateOut && screen -S background -d -L -Logfile "/tmp/rsync-$(date +%Y-%m-%d_%H-%M-%S.txt)" -m rsync -rtERvv --delete --exclude=".*/" /media/ext_storage/backups/ /mnt/nas_replicate_out
