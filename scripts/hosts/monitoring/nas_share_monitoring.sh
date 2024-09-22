5c7a1d5e6b0081f0874d5cdfc25e73df61ed8934
Ben <>
2024-09-21 17:07:34 -0700

    update mount monitoring

--- scripts/hosts/monitoring/nas_share_monitoring.sh
+++ scripts/hosts/monitoring/nas_share_monitoring.sh
@@ -2,7 +2,8 @@
 #   then the NAS may not be up when this server boots up. Thus the monitoring script can be used to attempt to re-mount the shared.
 #   Tested by using sudo unmount -l /mnt/ReplicateOut_master_control/ and then running this script. It reconnected the share as expected.
 #   Have not tested the failure scenario where NAS is offline when this run, for example.
-echo $(date '+%Y-%m-%d %H:%M:%S') Network share check > /mnt/ReplicateOut_master_control/monitoring/up.txt
+MONITORING_FILE=/mnt/nas_replicate_out/monitoring/up.txt
+echo $(date '+%Y-%m-%d %H:%M:%S') Network share check > $MONITORING_FILE
 
 if [ $? -eq 0 ]; then
     echo $(date '+%Y-%m-%d %H:%M:%S') Network share OK
@@ -11,7 +12,7 @@ else
     echo $(date '+%Y-%m-%d %H:%M:%S') Network share FAILED
     echo $(date '+%Y-%m-%d %H:%M:%S') Attempting to mount...
     sudo mount -a
-    echo $(date '+%Y-%m-%d %H:%M:%S') Network share check after re-mount > /mnt/ReplicateOut_master_control/monitoring/up.txt
+    echo $(date '+%Y-%m-%d %H:%M:%S') Network share check after re-mount > $MONITORING_FILE
     if [ $? -eq 0 ]; then
         echo $(date '+%Y-%m-%d %H:%M:%S') Network share mounted OK
         curl "https://uptime-kuma.ournetwork.ca/api/push/yISAMDTRaC?status=up&msg=OK&ping="
