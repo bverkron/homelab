c0f8cd9da0606bab8f828bd6306d337ee9d7cbaf
Ben <>
2024-09-21 17:17:27 -0700

    tweak backup script and set dry run for testing

--- scripts/backup/backup_to_nas.sh
+++ scripts/backup/backup_to_nas.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
-SHARE=ReplicateOut && screen -S background -d -L -Logfile "/tmp/rsync-$(date +%Y-%m-%d_%H-%M-%S.txt)" -m rsync -rtERvv --delete --exclude=".*/" /media/ext_storage/backups/ /mnt/nas_replicate_out/vector/backups/
+SHARE=ReplicateOut
+# screen -S background -d -L -Logfile "/tmp/rsync-$(date +%Y-%m-%d_%H-%M-%S.txt)" -m rsync -rtERvv --dryrun --delete --exclude=".*/" /media/ext_storage/backups/ /mnt/nas_replicate_out/vector/backups/
+rsync -rtERvv --dryrun --delete --exclude=".*/" /media/ext_storage/backups/ /mnt/nas_replicate_out/vector/backups/ &> /tmp/rsync-$(date +%Y-%m-%d_%H-%M-%S.txt)
