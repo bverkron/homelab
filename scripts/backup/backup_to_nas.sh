7a3de5149ff39b1186ba8c5285a68e77a45d3b70
Ben <>
2024-09-21 17:20:32 -0700

    fix rsync command error

--- scripts/backup/backup_to_nas.sh
+++ scripts/backup/backup_to_nas.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-SHARE=ReplicateOut
 # screen -S background -d -L -Logfile "/tmp/rsync-$(date +%Y-%m-%d_%H-%M-%S.txt)" -m rsync -rtERvv --dryrun --delete --exclude=".*/" /media/ext_storage/backups/ /mnt/nas_replicate_out/vector/backups/
-rsync -rtERvv --dryrun --delete --exclude=".*/" /media/ext_storage/backups/ /mnt/nas_replicate_out/vector/backups/ &> /tmp/rsync-$(date +%Y-%m-%d_%H-%M-%S.txt)
+rsync -rtERvv --dry-run --delete --exclude=".*/" /media/ext_storage/backups/ /mnt/nas_replicate_out/vector/backups/ &> /tmp/rsync-backup-$(date +%Y-%m-%d_%H-%M-%S.txt)
