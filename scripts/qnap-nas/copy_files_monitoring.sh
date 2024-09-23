1956fb16cbf18332055d3fcbbe203469eac8d013
Ben <>
2024-09-22 20:37:17 -0700

    qnap file transfer monitoring

---
+++ scripts/qnap-nas/copy_files_monitoring.sh
@@ -0,0 +1,8 @@
+while true
+do
+  AMOUNT=""
+  AMOUNT_TRANSFERED=$(du -h --max-depth=1 /share/single_disk_root/archive/ | grep /share/single_disk_root/archive/$ | sed 's/G.*//')
+  TOTAL=472 # Amount of source at the time. Doesn't change while transfer is happening to don't bother to have it autocalculated live, especially since it adds extra load onto the disk while transfer is happening a takes quite a while to calculate
+  awk -v amount=$AMOUNT_TRANSFERED -v total=$TOTAL -v date="$(date)" 'BEGIN { print  ( date " = " amount / total * 100 "%" ) }'
+  sleep 300
+done
