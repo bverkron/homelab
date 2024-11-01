492b8f9e2d2122075f205a0855377065a8347798
Bverkron <bverkron@asdf.asdf>
2024-11-01 10:38:13 -0700

    docker restart stacks refactor

--- scripts/hosts/restart_all_docker_compose_stacks.sh
+++ scripts/hosts/restart_all_docker_compose_stacks.sh
@@ -1,3 +1,20 @@
 # Simply restarting the container will not apply changes made in the compose file.
-# Need to re-CREATE the container by brining the compose stack down and backup
-for dir in ~/app-stacks/*; do (cd "$dir" && docker compose down && docker compose up -d); done
+# Need to re-CREATE the container by bringing the compose stack down and back up
+echo "Stopping stacks..."
+for dir in ~/app-stacks/*; do
+  (cd "$dir" && [ -f "compose.yaml" ] && docker compose down)
+done
+echo "Stop operation finished."
+
+read -p "Start stacks? [Y/y]" -n 1 -r
+echo ""
+if [[ $REPLY =~ ^[Yy]$ ]]
+then
+  echo "Starting stacks..."
+  for dir in ~/app-stacks/*; do
+    (cd "$dir" && [ -f "compose.yaml" ] && docker compose up -d)
+  done
+else
+  echo "Doing nothing"
+fi
+
